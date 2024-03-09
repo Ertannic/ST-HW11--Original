fd7142d3caff693f205a9addeab285d9eb9e156c
Erzhan <saralaev03@mail.ru>
2024-03-09 10:48:13 +0600

    Add standart first settings

--- ST-HW11-Саралаев ЕржанOriginal/ViewController.swift
+++ ST-HW11-Саралаев ЕржанOriginal/ViewController.swift
@@ -6,14 +6,75 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    private var elements = ["Авиарежим", "Wi-Fi", "Blotooth", "Сотовая связь", "Режим можема", "VPN"]
+   
+    // MARK: - Adding Elements
+    
+    private lazy var mainLabel: UILabel = {
+        let titleText = UILabel()
+        titleText.text = "Настройки"
+        titleText.textAlignment = .center
+        titleText.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        titleText.textColor = UIColor.black
+        
+        return titleText
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        return tableView
+        
+    }()
+    
+    // MARK: - Main screen Settings
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        setupElements()
+        setupConstaints()
+        
+    }
+    
+    // MARK: - Add setups
+
+    func setupElements() {
+        view.addSubview(mainLabel)
+        view.addSubview(tableView)
     }
 
+    func setupConstaints() {
+        
+        mainLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(-80)
+            make.centerX.equalToSuperview()
+        }
 
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(mainLabel.snp.bottom).offset(16)
+            make.right.bottom.left.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Table Functions
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
 }
 
